2281cc8036e35e226eb8df7316e334c2e0fd1a01
rolandoandrade <rolandoandradefernandez@gmail.com>
2021-02-07 11:20:37 -0400

    Starting form

---
+++ apps/mob-rest-app/mob-rest-client/src/modules/mob/views/MobView.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <v-card max-width="800" class="mx-auto pa-2 card-rounded" outlined>
+      <v-form class="pa-4">
+        <v-text-field prepend-icon="mdi-book" label="Título"></v-text-field>
+        <v-row no-gutters>
+          <v-text-field class="mr-2" prepend-icon="mdi-account" label="Nombre del autor"></v-text-field>
+          <v-text-field class="ml-2" prepend-icon="mdi-account" label="Apellido del autor"></v-text-field>
+        </v-row>
+        <v-row no-gutters justify="end">
+          <v-btn color="primary" rounded large block><v-icon class="mr-4">mdi-magnify</v-icon>BUSCAR LIBROS</v-btn>
+        </v-row>
+
+      </v-form>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import {Prop} from 'vue-property-decorator';
+
+@Component({
+  name: 'mob-view',
+})
+export default class MobView extends Vue {
+
+}
+</script>
+
+<style scoped>
+.card-rounded{
+  border-radius: 35px !important;
+}
+</style>
---
+++ apps/mob-rest-app/mob-rest-client/src/plugins/vuetify.ts
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+import es from 'vuetify/src/locale/es';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+      options: {
+        customProperties: true,
+      },
+    themes: {
+      light: {
+        primary: '#2196F3',
+        secondary: '#1870b4',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107'
+      },
+    },
+  },
+    lang: {
+      locales: { es },
+      current: 'es',
+    },
+});
